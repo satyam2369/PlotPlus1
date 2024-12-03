68e2619f5b2149af99de86e5f929b36ec16a3b9e
Anju-2004 <ad.jatti.2003@gmail.com>
2024-12-03 20:27:27 +0530

    removed

--- Backend/model/admin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const AdminSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-})
-
-module.exports = mongoose.model("Admin", AdminSchema);
--- Backend/model/storyViews.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const viewSchema = mongoose.Schema({
-    uid: String,
-    storyid: String,
-})
-
-module.exports = mongoose.model("storyViews", viewSchema);
